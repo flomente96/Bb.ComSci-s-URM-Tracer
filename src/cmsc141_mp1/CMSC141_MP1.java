8a45033ddca43435df14fc58c9bf48865a764362
faielomente <frlomente@up.edu.ph>
2016-03-08 19:20:08 +0800

    added comments to the code

--- src/cmsc141_mp1/CMSC141_MP1.java
+++ src/cmsc141_mp1/CMSC141_MP1.java
@@ -26,26 +26,24 @@ public class CMSC141_MP1 {
     static int current_command = 1;
     
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        // TODO code application logic here
-//        Scanner scan = new Scanner(System.in);
         String fileName = "mp1.in";
-        String[] lines = open_file(fileName);
-        int numberOfLines = readLines();
+        String[] textData = open_file(fileName);    //Holds the text data the file contains.
+        int numberOfLines = readLines();    //Used as stoping condition 
         File file = create_file();
         FileWriter writer = new FileWriter(file);
-        ArrayList data = new ArrayList();
+        ArrayList data = new ArrayList();   //Holds the state of the URM in an ArrayList
         
-        String[] temp_list = lines[0].split("\\s+");
-        for(int i = 0; i < temp_list.length; i++){
-            data.add(Integer.parseInt(temp_list[i]));
+        String[] initState = textData[0].split("\\s+"); //Holds the initial state of the URM
+        for(int i = 0; i < initState.length; i++){  //Converted the initial state to ArrayList for easy manipulation
+            data.add(Integer.parseInt(initState[i]));
         }
         writer.write(to_string(data));
         writer.write("\n");
         
-//        System.out.println("-------------------------");
-        while (current_command < numberOfLines){
-            ArrayList parsed = parse_line(lines[current_command]);
+        while (current_command < numberOfLines){//Parses each command, execute them, updates the state of the URM and writes it
+            ArrayList parsed = parse_line(textData[current_command]);
             data = execute(data, parsed);
+            System.out.println(to_string(data));
             writer.write(to_string(data));
             writer.write("\n");
         }
@@ -53,6 +51,9 @@ public class CMSC141_MP1 {
         writer.close();
     }
     public static File create_file() throws IOException{
+        /*
+        *Creates a File
+        */
         File file = new File("mp1.out");
         file.createNewFile();
         
@@ -60,6 +61,9 @@ public class CMSC141_MP1 {
     }
     
     public static String[] open_file(String fileName) throws FileNotFoundException, IOException{
+        /*
+        *Opens a file and extract its contents.
+        */
         FileReader fr = new FileReader(fileName);
         BufferedReader textReader = new BufferedReader(fr);
         
@@ -74,6 +78,9 @@ public class CMSC141_MP1 {
     }
     
     public static int readLines() throws FileNotFoundException, IOException{
+        /*
+        *Counts the total number of lines the file has.
+        */
         FileReader fr = new FileReader("mp1.in");
         BufferedReader bf = new BufferedReader(fr);
         
@@ -88,6 +95,10 @@ public class CMSC141_MP1 {
     }
     
     public static ArrayList parse_line(String line){
+        /*
+        *Distinguishes between character and number a line contains. And return
+        *it as an ArrayList.
+        */
         String[] temp = line.split("\\s+");
         ArrayList parsed = new ArrayList();
         
@@ -99,10 +110,13 @@ public class CMSC141_MP1 {
                 parsed.add(Integer.parseInt(temp[i]));
             }
         }
-//        System.out.println("Parsed: " + parsed.toString());
+        System.out.println("Parsed: " + parsed.toString());
         return parsed;
     }
     public static String to_string(ArrayList data){
+        /*
+        *Prints the contents of the ArrayList as a string.
+        */
         StringBuilder str_builder = new StringBuilder();
         
         for(int i = 0; i < data.size(); i++){
@@ -113,9 +127,13 @@ public class CMSC141_MP1 {
         return str_builder.toString();
     }
     
-    public static ArrayList execute(ArrayList current_state, ArrayList command){
+    public static ArrayList execute(ArrayList state, ArrayList command){
+        /*
+        *Executes each command found in the file. It also updates the states and
+        *returns it as an ArrayList.
+        */
         if(command.get(0).toString().equals("J")){
-            if(current_state.get((int)command.get(1)).equals(current_state.get((int)command.get(2)))){
+            if(state.get((int)command.get(1)).equals(state.get((int)command.get(2)))){
                 current_command = (int) command.get(3);
             }
             else{
@@ -124,22 +142,22 @@ public class CMSC141_MP1 {
         }
         else if(command.get(0).toString().equals("S")){
             int temp_i = (int)command.get(1);
-            int temp = (int)current_state.get(temp_i);
-            current_state.set(temp_i, temp + 1);
+            int temp = (int)state.get(temp_i);
+            state.set(temp_i, temp + 1);
             current_command++;
         }
         else if(command.get(0).toString().equals("C")){
             int from = (int)command.get(1);
             int to = (int)command.get(2);
-            int temp = (int)current_state.get(from);
-            current_state.set(to, temp);
+            int temp = (int)state.get(from);
+            state.set(to, temp);
             current_command++;
         }
         else if(command.get(0).toString().equals("Z")){
             int temp_i = (int)command.get(1);
-            current_state.set(temp_i, 0);
+            state.set(temp_i, 0);
             current_command++;
         }
-       return current_state;
+       return state;
     }
 }
